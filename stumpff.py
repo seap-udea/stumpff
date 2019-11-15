f0639dc44077006cbc812e0ec27f2bb76536cf35
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2019-11-15 12:25:41 -0500

    Commit

--- stumpff.py
+++ stumpff.py
@@ -1,16 +1,15 @@
+from math import factorial
+from math import cos,sin,cosh,sinh
 def series_stumpff(t,k,N=15):
-    from math import factorial
     sk=lambda n:t/((2*n+k+1)*(2*n+k+2))*(1-sk(n+1)) if n<N else 0
     return (1-sk(0))/factorial(k)
 
 def error_series_stumpff(t,k,N):
     Pi=lambda N,k:(2*N+k+1)*(2*N+k+2)*Pi(N-1,k) if N>=0 else 1
-    from math import factorial
     error=abs(t**N/Pi(N,k)/factorial(k))
     return error
 
 def series_stumpff_analytic(t):
-    from math import cos,sin,cosh,sinh
     y=abs(t)**0.5
     c0=cos(y) if t>=0 else cosh(y)
     c1=sin(y)/y if t>=0 else sinh(y)/y
