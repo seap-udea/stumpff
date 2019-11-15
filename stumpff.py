db9e728355a2a23141bc0c4112b73e6614f25c69
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2019-11-15 15:31:17 -0500

    Commit

--- stumpff.py
+++ stumpff.py
@@ -3,16 +3,16 @@ from math import factorial
 from math import cos,sin,cosh,sinh
 
 @functools.lru_cache(maxsize=128)
-def series_stumpff(t,k,N=15):
+def ck(t,k,N=20):
     sk=lambda n:t/((2*n+k+1)*(2*n+k+2))*(1-sk(n+1)) if n<N else 0
     return (1-sk(0))/factorial(k)
 
-def error_series_stumpff(t,k,N):
+def error_ck(t,k,N):
     Pi=lambda N,k:(2*N+k+1)*(2*N+k+2)*Pi(N-1,k) if N>=0 else 1
     error=abs(t**N/Pi(N,k)/factorial(k))
     return error
 
-def series_stumpff_analytic(t):
+def ck_analytic(t):
     y=abs(t)**0.5
     c0=cos(y) if t>=0 else cosh(y)
     c1=sin(y)/y if t>=0 else sinh(y)/y
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from numpy import linspace
 for t in linspace(-100,100):
 
     #Analytical values
-    c0,c1,c2,c3=series_stumpff_analytic(t)
+    c0,c1,c2,c3=ck_analytic(t)
     
     #Search for the optimum value of N
     k=0
@@ -18,8 +18,8 @@ for t in linspace(-100,100):
     N=0
     while error>delta:
         N+=1
-        error=error_series_stumpff(t,k,N)
-    delta_c0=abs(series_stumpff(t,k,N)-c0)
+        error=error_ck(t,k,N)
+    delta_c0=abs(ck(t,k,N)-c0)
     errors+=[delta_c0]
     
     #Search for the optimum value of N
@@ -28,8 +28,8 @@ for t in linspace(-100,100):
     N=0
     while error>delta:
         N+=1
-        error=error_series_stumpff(t,k,N)
-    delta_c1=abs(series_stumpff(t,k,N)-c1)
+        error=error_ck(t,k,N)
+    delta_c1=abs(ck(t,k,N)-c1)
     errors+=[delta_c1]
 
     #Search for the optimum value of N
@@ -38,8 +38,8 @@ for t in linspace(-100,100):
     N=0
     while error>delta:
         N+=1
-        error=error_series_stumpff(t,k,N)
-    delta_c2=abs(series_stumpff(t,k,N)-c2)
+        error=error_ck(t,k,N)
+    delta_c2=abs(ck(t,k,N)-c2)
     errors+=[delta_c2]
 
     #Search for the optimum value of N
@@ -48,8 +48,8 @@ for t in linspace(-100,100):
     N=0
     while error>delta:
         N+=1
-        error=error_series_stumpff(t,k,N)
-    delta_c3=abs(series_stumpff(t,k,N)-c3)
+        error=error_ck(t,k,N)
+    delta_c3=abs(ck(t,k,N)-c3)
     errors+=[delta_c3]
 
 print(f"\tMaximum achieved error: {max(errors)}")
@@ -60,8 +60,8 @@ print(f"Timing numerical stumpff series...")
 from timeit import Timer
 Nr=1000
 t=-10
-all_stumpffs=lambda t:[series_stumpff(t,k) for k in range(4)]
+all_stumpffs=lambda t:[ck(t,k,N) for k in range(4)]
 time_series=Timer("all_stumpffs(t)",globals=globals()).timeit(Nr)/Nr*1e6
-time_trigo=Timer("series_stumpff_analytic(t)",globals=globals()).timeit(Nr)/Nr*1e6
+time_trigo=Timer("ck_analytic(t)",globals=globals()).timeit(Nr)/Nr*1e6
 print(f"\tRunning time for series: {time_series:g} usec")
 print(f"\tRunning time for trigonometric: {time_trigo:g} usec")
