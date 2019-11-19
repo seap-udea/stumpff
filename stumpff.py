c146b47690541bb4cb34940ba04438a24e6e8dda
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2019-11-19 15:49:17 -0500

    Commit

--- stumpff.py
+++ stumpff.py
@@ -2,20 +2,32 @@ from math import cos,sin,cosh,sinh
 
 import functools
 from math import factorial
+
 @functools.lru_cache(maxsize=128)
-def ck(t,k,N=20):
+def ck(t,k,N=1,delta=1e-15):
+    while abs(t**N/factorial(2*N+k+2))>delta:N+=5
+    sk=lambda n:t/((2*n+k+1)*(2*n+k+2))*(1-sk(n+1)) if n<N else 0
+    return (1-sk(0))/factorial(k)
+
+@functools.lru_cache(maxsize=128)
+def ck_noerror(t,k,N=20):
     sk=lambda n:t/((2*n+k+1)*(2*n+k+2))*(1-sk(n+1)) if n<N else 0
     return (1-sk(0))/factorial(k)
 
 def error_ck(t,k,N):
-    Pi=lambda N,k:(2*N+k+1)*(2*N+k+2)*Pi(N-1,k) if N>=0 else 1
-    error=abs(t**N/Pi(N,k)/factorial(k))
-    return error
+    return abs(t**N/factorial(2*N+k+2))
 
 def ck_analytic(t):
-    y=abs(t)**0.5
-    c0=cos(y) if t>=0 else cosh(y)
-    c1=sin(y)/y if t>=0 else sinh(y)/y
-    c2=(1-cos(y))/y**2 if t>=0 else -(1-cosh(y))/y**2
-    c3=(y-sin(y))/y**3 if t>=0 else -(y-sinh(y))/y**3
+    if t==0:
+        c0=1
+        c1=1
+        c2=1/2
+        c3=1/6
+    else:
+        y=abs(t)**0.5
+        c0=cos(y) if t>=0 else cosh(y)
+        c1=sin(y)/y if t>=0 else sinh(y)/y
+        c2=(1-cos(y))/y**2 if t>=0 else -(1-cosh(y))/y**2
+        c3=(y-sin(y))/y**3 if t>=0 else -(y-sinh(y))/y**3
     return c0,c1,c2,c3
+
--- test.py
+++ test.py
@@ -6,61 +6,25 @@ print("Computing stumpff series across two orders of magnitude...")
 #######################################################################
 errors=[]
 delta=1e-14
-from numpy import linspace
-for t in linspace(-100,100):
 
+#Range to explore
+from numpy import log10,logspace,linspace,hstack
+trange1=logspace(-5,0,10)
+trange2=linspace(0,100,200)
+for t in hstack((-trange2,-trange1,trange1,trange2)):
     #Analytical values
     c0,c1,c2,c3=ck_analytic(t)
-    
-    #Search for the optimum value of N
-    k=0
-    error=1
-    N=0
-    while error>delta:
-        N+=1
-        error=error_ck(t,k,N)
-    delta_c0=abs(ck(t,k,N)-c0)
-    errors+=[delta_c0]
-    
-    #Search for the optimum value of N
-    k=1
-    error=1
-    N=0
-    while error>delta:
-        N+=1
-        error=error_ck(t,k,N)
-    delta_c1=abs(ck(t,k,N)-c1)
-    errors+=[delta_c1]
-
-    #Search for the optimum value of N
-    k=2
-    error=1
-    N=0
-    while error>delta:
-        N+=1
-        error=error_ck(t,k,N)
-    delta_c2=abs(ck(t,k,N)-c2)
-    errors+=[delta_c2]
-
-    #Search for the optimum value of N
-    k=3
-    error=1
-    N=0
-    while error>delta:
-        N+=1
-        error=error_ck(t,k,N)
-    delta_c3=abs(ck(t,k,N)-c3)
-    errors+=[delta_c3]
-
-print(f"\tMaximum achieved error: {max(errors)}")
+    errors+=[abs(ck(t,0)-c0),abs(ck(t,1)-c1),
+             abs(ck(t,2)-c2),abs(ck(t,3)-c3)]
+print(f"\tMaximum achieved error: {max(errors):.1e}")
 
 #######################################################################
 print(f"Timing numerical stumpff series...")
 #######################################################################
 from timeit import Timer
 Nr=1000
-t=-10
-all_stumpffs=lambda t:[ck(t,k,N) for k in range(4)]
+t=1
+all_stumpffs=lambda t:[ck(t,k) for k in range(4)]
 time_series=Timer("all_stumpffs(t)",globals=globals()).timeit(Nr)/Nr*1e6
 time_trigo=Timer("ck_analytic(t)",globals=globals()).timeit(Nr)/Nr*1e6
 print(f"\tRunning time for series: {time_series:g} usec")
